22fc00b10e0b993787c3f3db4af33f69fd48fa88
Gareth Wilcock <gewilcock@gmail.com>
2014-03-25 22:44:17 +0200

    Functional AI combat

---
+++ Robot Ultrarena/Assets/Scripts/MechsplosionScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MechsplosionScript : MonoBehaviour {
+	Light flareLight;
+	ParticleSystem particles;
+	float deathTime;
+	public Transform myParent;
+	// Use this for initialization
+	void Start () {
+		deathTime=Time.time+5.09f;
+		flareLight = GetComponent<Light>();
+		particles=GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		flareLight.range+=10*Time.deltaTime;
+		if(myParent!=null){
+			transform.position=myParent.position;
+			transform.rotation=myParent.rotation;
+		}
+
+		if(Time.time>deathTime){
+			flareLight.enabled=false;
+			if(!particles.IsAlive ()){GameObject.Destroy (this.gameObject);}
+		}
+	}
+}
