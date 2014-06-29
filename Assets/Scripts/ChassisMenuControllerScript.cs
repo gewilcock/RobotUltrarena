afae3cb669639cf11e9f9c9d247486508609f7a0
Andre Odendaal <aodendaal@gmail.com>
2014-06-29 22:37:27 +0200

    Added barrier transparency script and some defensive coding in some scripts

--- Assets/Scripts/ChassisMenuControllerScript.cs
+++ Assets/Scripts/ChassisMenuControllerScript.cs
@@ -1,52 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ChassisMenuControllerScript : MonoBehaviour {
-	public TextMesh Mechname;
-	public TextMesh Mechstats;
-	// Use this for initialization
-	int mechIndex;
+public class ChassisMenuControllerScript : MonoBehaviour
+{
+    public TextMesh Mechname;
+    public TextMesh Mechstats;
+    // Use this for initialization
+    int mechIndex;
 
-	void Start () {
+    void Start()
+    {
+        var spawner = GarageSpawnerScript.Spawner;
+        if (spawner.wControl != null)
+        {
+            var mechChassis = PrefabLibraryScript.library.mechChassis;
+            for (int i = 0; i < mechChassis.Length; i++)
+            {
+                if (spawner.wControl.gameObject.name.IndexOf(mechChassis[i].name) > -1)
+                {
+                    mechIndex = i;
+                }
+            }
+        }
+    }
 
-			for (int l=0; l<PrefabLibraryScript.library.mechChassis.Length; l++)
-			{
-				
-				if(GarageSpawnerScript.Spawner.wControl.gameObject.name.IndexOf(PrefabLibraryScript.library.mechChassis[l].name)>-1)
-				{
-					mechIndex = l;
-				}
-				
-				
-			}
-		
-	}
+    void cycleChassis(int shiftdirection)
+    {
+        mechIndex += shiftdirection;
 
-	void cycleChassis(int shiftdirection)
-	{
-		mechIndex += shiftdirection;
-		
-		if(mechIndex<0)
-			mechIndex = PrefabLibraryScript.library.mechChassis.Length-1;
-		else if(mechIndex==PrefabLibraryScript.library.mechChassis.Length)
-			mechIndex = 0;
+        if (mechIndex < 0)
+            mechIndex = PrefabLibraryScript.library.mechChassis.Length - 1;
+        else if (mechIndex == PrefabLibraryScript.library.mechChassis.Length)
+            mechIndex = 0;
 
-		GameDataManagerScript.GameDataManager.players[0].myMech = PrefabLibraryScript.library.mechChassis[mechIndex];
+        GameDataManagerScript.GameDataManager.players[0].myMech = PrefabLibraryScript.library.mechChassis[mechIndex];
 
-	}
+    }
 
-	void updateText () {
-		string namestring = GarageSpawnerScript.Spawner.mControl.gameObject.name;
-		Mechname.text = namestring.Substring (0,namestring.Length-7);
-		Mechstats.text = PrefabLibraryScript.library.getDescription(Mechname.text)+"\n\n";
-		Mechstats.text += "Armour: \t\t\t"+ GarageSpawnerScript.Spawner.mControl.maxArmour.ToString () + "\n"+
-			"Mass: \t\t\t" + (GarageSpawnerScript.Spawner.mControl.rigidbody.mass/1000).ToString () + " tons\n" +
-				"Max Speed: \t\t" + GarageSpawnerScript.Spawner.cMotor.movement.maxForwardSpeed.ToString () + " m/s\n"+
-				"Max turn rate: \t\t" + GarageSpawnerScript.Spawner.mControl.mechTurnRate.ToString () + " degrees/s\n" +
-				"Energy Cache: \t\t"+GarageSpawnerScript.Spawner.wControl.maxEnergyLevel.ToString () +" units\n"+
-				"Cooling Rate: \t\t"+GarageSpawnerScript.Spawner.wControl.heatSinkRate.ToString () +" units/s\n\n"+
-				"Weapon Slots: \t\t"+GarageSpawnerScript.Spawner.wControl.weaponList.Length.ToString ()+"\n"+
-				"Heavy Weapon Slots: \t"+GarageSpawnerScript.Spawner.wControl.heavyWeaponList.Length.ToString ()+"\n"+
-				"Module Slots: \t\t" + GarageSpawnerScript.Spawner.wControl.moduleList.Length.ToString ();
-	}
+    void updateText()
+    {
+        string namestring = GarageSpawnerScript.Spawner.mControl.gameObject.name;
+        Mechname.text = namestring.Substring(0, namestring.Length - 7);
+        Mechstats.text = PrefabLibraryScript.library.getDescription(Mechname.text) + "\n\n";
+        Mechstats.text += "Armour: \t\t\t" + GarageSpawnerScript.Spawner.mControl.maxArmour.ToString() + "\n" +
+            "Mass: \t\t\t" + (GarageSpawnerScript.Spawner.mControl.rigidbody.mass / 1000).ToString() + " tons\n" +
+                "Max Speed: \t\t" + GarageSpawnerScript.Spawner.cMotor.movement.maxForwardSpeed.ToString() + " m/s\n" +
+                "Max turn rate: \t\t" + GarageSpawnerScript.Spawner.mControl.mechTurnRate.ToString() + " degrees/s\n" +
+                "Energy Cache: \t\t" + GarageSpawnerScript.Spawner.wControl.maxEnergyLevel.ToString() + " units\n" +
+                "Cooling Rate: \t\t" + GarageSpawnerScript.Spawner.wControl.heatSinkRate.ToString() + " units/s\n\n" +
+                "Weapon Slots: \t\t" + GarageSpawnerScript.Spawner.wControl.weaponList.Length.ToString() + "\n" +
+                "Heavy Weapon Slots: \t" + GarageSpawnerScript.Spawner.wControl.heavyWeaponList.Length.ToString() + "\n" +
+                "Module Slots: \t\t" + GarageSpawnerScript.Spawner.wControl.moduleList.Length.ToString();
+    }
 }
--- Assets/Scripts/Control objects and input/MechSpawnerScript.cs
+++ Assets/Scripts/Control objects and input/MechSpawnerScript.cs
@@ -1,114 +1,141 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MechSpawnerScript : MonoBehaviour {
+public class MechSpawnerScript : MonoBehaviour
+{
 
-	public GameObject PlayerCameraPrefab;
-	public GameObject HUDObject;
+    public GameObject PlayerCameraPrefab;
+    public GameObject HUDObject;
+	public Transform playerStartLocation;
+
+    // Use this for initialization
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+        SpawnPlayers();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SpawnPlayers()
+    {
+
+        MechData[] players = GameDataManagerScript.GameDataManager.players;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+
+            MechData weh = players[i];
+            GameObject newMech;
+
+            if (weh.isPlayer)
+            {
+                var playerCamera = (GameObject)Instantiate(PlayerCameraPrefab, Vector3.zero, Quaternion.identity);
+				newMech = (GameObject)Instantiate(weh.myMech, playerStartLocation.position, playerStartLocation.rotation);
+                MechInputHandler handler = (MechInputHandler)newMech.AddComponent("MechInputHandler");
+
+                handler.absThrottle = GameDataManagerScript.GameDataManager.playerOptions.useAbsoluteThrottle;
+
+                Invoke("SpawnHUD", 0.1f);
+
+				var northBarrier = GameObject.Find("NorthBarrier");
+				northBarrier.GetComponent<BarrierScript>().player = playerCamera.transform;
+				var southBarrier = GameObject.Find("SouthBarrier");
+				southBarrier.GetComponent<BarrierScript>().player = playerCamera.transform;
+				var eastBarrier = GameObject.Find("EastBarrier");
+				eastBarrier.GetComponent<BarrierScript>().player = playerCamera.transform;
+				var westBarrier = GameObject.Find("WestBarrier");
+				westBarrier.GetComponent<BarrierScript>().player = playerCamera.transform;
+
+            }
+            else
+            {
+                newMech = (GameObject)Instantiate(weh.myMech, new Vector3(UnityEngine.Random.Range(30, 2200), 500, UnityEngine.Random.Range(30, 2200)), Quaternion.identity);
+                newMech.AddComponent("MechAIHandler");
+            }
+            RaycastHit groundhit;
+            Physics.Raycast(newMech.transform.position, Vector3.down, out groundhit, 1000f);
+            newMech.transform.position = groundhit.point;
+
+            WeaponController wControl = newMech.GetComponent<WeaponController>();
+
+            for (int w = 0; w < weh.myWeapons.Length; w++)
+            {
+                if (weh.myWeapons[w] != null)
+                {
+
+                    GameObject newGun = (GameObject)Instantiate(weh.myWeapons[w], Vector3.zero, Quaternion.identity);
+
+                    int pivotIndex;
+                    float mountRotation;
+
+                    if (w < wControl.weaponsPerGroup)
+                    {
+                        pivotIndex = 0;
+                        mountRotation = w + wControl.weaponsPerGroup - 1;
+                    }
+                    else
+                    {
+                        pivotIndex = 1;
+                        mountRotation = w + (2 * wControl.weaponsPerGroup) - 1;
+                    }
 
 
-	// Use this for initialization
-	void Awake () {
+                    newGun.transform.parent = wControl.aimPivots[pivotIndex].transform;
+                    newGun.transform.localPosition = Vector3.zero;
+                    newGun.transform.localEulerAngles = new Vector3(0, 0, -180 + (180 * mountRotation));
 
-	}
+                    wControl.weaponList[w] = newGun.GetComponent<WeaponScript>();
 
-	void Start(){
-		
-		SpawnPlayers ();
+                }
+            }
 
-	}
+            for (int w = 0; w < weh.myHeavyWeapons.Length; w++)
+            {
+                if (weh.myHeavyWeapons[w] != null)
+                {
+                    if (w < wControl.HWMounts.Length)
+                    {
+                        GameObject newGun = (GameObject)Instantiate(weh.myHeavyWeapons[w], wControl.HWMounts[w].transform.position, Quaternion.identity);
 
-	// Update is called once per frame
-	void Update () {
-	
-	}
+                        newGun.transform.parent = wControl.HWMounts[w].transform;
 
-	public void SpawnPlayers(){
+                        wControl.heavyWeaponList[w] = newGun.GetComponent<WeaponScript>();
+                    }
+                }
+            }
 
-		MechData[] players = GameDataManagerScript.GameDataManager.players;
+            for (int w = 0; w < weh.myModules.Length; w++)
+            {
+                if (weh.myModules[w] != null)
+                {
+                    if (w < wControl.moduleList.Length)
+                    {
+                        GameObject newModule = (GameObject)Instantiate(weh.myModules[w], wControl.transform.position, Quaternion.identity);
 
-		for(int i =0; i<players.Length;i++){
+                        newModule.transform.parent = wControl.transform;
+                        wControl.moduleList[w] = newModule.GetComponent<AbilityModuleScript>();
+                        wControl.moduleList[w].wControl = wControl;
+                    }
+                }
+            }
 
-			MechData weh = players[i];
-			GameObject newMech;
+        }
+    }
 
-			if(weh.isPlayer){
-				Instantiate (PlayerCameraPrefab,Vector3.zero,Quaternion.identity);
-				newMech = (GameObject)Instantiate(weh.myMech,new Vector3(50,500,50),Quaternion.identity);
-				MechInputHandler handler = (MechInputHandler)newMech.AddComponent ("MechInputHandler");
-
-				handler.absThrottle = GameDataManagerScript.GameDataManager.playerOptions.useAbsoluteThrottle;
-
-				Invoke ("SpawnHUD",0.1f);
-
-			}
-			else{
-				newMech = (GameObject)Instantiate (weh.myMech,new Vector3(UnityEngine.Random.Range (30,2200),500,UnityEngine.Random.Range(30,2200)),Quaternion.identity);
-				newMech.AddComponent ("MechAIHandler");
-			}
-			RaycastHit groundhit;
-			Physics.Raycast (newMech.transform.position,Vector3.down,out groundhit,1000f);
-			newMech.transform.position=groundhit.point;
-
-			WeaponController wControl = newMech.GetComponent<WeaponController>();
-
-			for(int w=0; w<weh.myWeapons.Length;w++){
-				if(weh.myWeapons[w]!=null){
-
-					GameObject newGun=(GameObject)Instantiate (weh.myWeapons[w],Vector3.zero,Quaternion.identity);
-
-					int pivotIndex;
-					float mountRotation;
-
-					if(w<wControl.weaponsPerGroup){
-						pivotIndex=0;
-						mountRotation=w+wControl.weaponsPerGroup-1;
-					}
-					else{
-						pivotIndex=1;
-						mountRotation=w+(2*wControl.weaponsPerGroup)-1;
-					}
-
-
-					newGun.transform.parent=wControl.aimPivots[pivotIndex].transform;
-					newGun.transform.localPosition=Vector3.zero;
-					newGun.transform.localEulerAngles = new Vector3(0,0,-180+(180*mountRotation));
-
-					wControl.weaponList[w]=newGun.GetComponent<WeaponScript>();
-
-				}
-			}
-
-			for(int w=0; w<weh.myHeavyWeapons.Length;w++){
-				if(weh.myHeavyWeapons[w]!=null){
-					if(w<wControl.HWMounts.Length){
-					GameObject newGun=(GameObject)Instantiate (weh.myHeavyWeapons[w],wControl.HWMounts[w].transform.position,Quaternion.identity);
-
-					newGun.transform.parent=wControl.HWMounts[w].transform;
-					
-					wControl.heavyWeaponList[w]=newGun.GetComponent<WeaponScript>();
-					}
-				}
-			}
-
-			for(int w=0; w<weh.myModules.Length;w++){
-				if(weh.myModules[w]!=null){
-					if(w<wControl.moduleList.Length){
-						GameObject newModule=(GameObject)Instantiate (weh.myModules[w],wControl.transform.position,Quaternion.identity);
-						
-						newModule.transform.parent=wControl.transform;
-						wControl.moduleList[w]=newModule.GetComponent<AbilityModuleScript>();
-						wControl.moduleList[w].wControl=wControl;
-					}
-				}
-			}
-
-		}
-	}
-
-	void SpawnHUD(){
-		GameObject HUD = (GameObject)Instantiate (HUDObject,Camera.main.transform.position,Camera.main.transform.rotation);
-		HUD.transform.parent=Camera.main.transform;
-
-	}
+    void SpawnHUD()
+    {
+        GameObject HUD = (GameObject)Instantiate(HUDObject, Camera.main.transform.position, Camera.main.transform.rotation);
+        HUD.transform.parent = Camera.main.transform;
+    }
 }
